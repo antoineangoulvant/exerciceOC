72ac1790dce6a1be3e4defd1d5af018385ed8f47
Angoulvant Antoine <antoine.angoulvant@yahoo.fr>
2018-01-31 16:45:59 +0100

    Ajout de la classe Test

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+	public static void main(String[] args){
+		Personne personne1;
+		personne1 = new Personne("Angoulvant","Antoine",21,1.90);
+		System.out.println(personne1.toString());
+	}
+}
